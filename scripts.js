ea3982f3df75a4a98b314ecab956eabc8c4f104c
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-05-10 05:30:16 +0200

    Added code for when the input contains invalif characters, it shold return an error message

--- scripts.js
+++ scripts.js
@@ -7,10 +7,18 @@ form.addEventListener("submit", (event) => {
   const dividend = entries.get("dividend");
   const divider = entries = entries.get("divider");
 
-//check if inputs are empty
-if (!dividend || !divider) {
-  //result.classlist.add("error message")
-  result.innertext = 
-  "Division not performed. Both values are required in inputs. Try again.";
-  return;
+// check if inputs are empty
+  if (!dividend || !divider) {
+    //result.classList.add("error-message")
+    result.innerText =
+      "Division not performed. Both values are required in inputs. Try again.";
+    return;
+  }
+
+  //checks if inputs contain invalid characters
+  const validInputRegex = /^[0-9]+$/;
+if (!validInputRegex.test(dividend) || !validInputRegex.test(divider)) {
+    result.classList.add("critical-error");
+    result,innerText = "something critical went wrong. Please reload the page.";
+    return;
 }
